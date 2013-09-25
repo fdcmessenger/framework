5cf161af17615936dd4a25e17d57bcc58cd86161
fdcmessenger <fdcmessenger@gmail.com>
2013-09-25 16:20:09 +0800

    加入带有默认id定义的抽象实体基类。

---
+++ model/src/main/java/org/sbbs/base/model/BaseIDEntity.java
@@ -0,0 +1,24 @@
+package org.sbbs.base.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseIDEntity extends BaseObject {
+
+	private static final long serialVersionUID = 2419117914596882542L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
